73c1af5b72c2f0a234c87cfd5f2badbaddca2903
MisanthropicBit <alexander.asp.bock@gmail.com>
2015-08-24 21:34:54 +0200

    Added build functions for tests

---
+++ cmake/Functions.cmake
@@ -0,0 +1,21 @@
+# Function for building a cvx example
+function(cvx_build_example target needs_opencv)
+    add_executable(${target} ${target}.cpp ${CVX_SOURCES})
+    set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/examples/bin)
+
+    if(needs_opencv AND OpenCV_FOUND)
+        target_link_libraries(${target} ${OpenCV_LIBS})
+    endif()
+endfunction()
+
+# Function for building a cvx test
+function(cvx_build_test target needs_opencv)
+    add_executable(${target} ${target}.cpp ${CVX_SOURCES})
+    set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/test/bin)
+
+    if(needs_opencv AND OpenCV_FOUND)
+        target_link_libraries(${target} ${OpenCV_LIBS})
+    endif()
+
+    add_test(${target} ${PROJECT_SOURCE_DIR}/tests/bin/${target})
+endfunction()
