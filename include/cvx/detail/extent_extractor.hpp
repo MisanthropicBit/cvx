f40579cad4f898196a2f419eff2a42ce7fbab99b
MisanthropicBit <alexander.asp.bock@gmail.com>
2015-09-30 10:21:10 +0200

    Added missing header

---
+++ include/cvx/detail/extent_extractor.hpp
@@ -0,0 +1,17 @@
+#ifndef CVX_EXTENT_EXTRACTOR_HPP
+#define CVX_EXTENT_EXTRACTOR_HPP
+
+#include "cvx/detail/extractor.hpp"
+
+namespace cvx {
+    namespace detail {
+        class extent_extractor final : public extractor {
+            public:
+                void initialise() override;
+                void update(std::size_t x, std::size_t y, connected_component& cc) override;
+                void finalise(connected_component& cc) override;
+        };
+    } // detail
+} // cvx
+
+#endif // CVX_EXTENT_EXTRACTOR_HPP
