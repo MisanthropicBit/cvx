cdde8c105195f0911186af79199255b8b2138503
MisanthropicBit <alexander.asp.bock@gmail.com>
2015-10-07 21:35:01 +0200

    Fixed error in valgrind script

--- tests/valgrind_tests.sh
+++ tests/valgrind_tests.sh
@@ -2,7 +2,6 @@
 
 for filename in bin/*; do
     if [ -f "$filename" -a -x "$filename" ]; then
-        echo "$filename"
-        #valgrind --leak-check=full --error-exitcode=1 ./"$filename"
+        valgrind --leak-check=full --error-exitcode=1 ./"$filename"
     fi
 done
