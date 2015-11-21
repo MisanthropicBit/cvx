81c29afdba1b719dcecbcde14d106481a198b298
MisanthropicBit <alexander.asp.bock@gmail.com>
2015-11-21 15:30:00 +0100

    Brought in changes from master to the example/test CMake functions

--- cmake/Functions.cmake
+++ cmake/Functions.cmake
@@ -1,19 +1,19 @@
 # Function for building a cvx example
-function(cvx_build_example target needs_opencv)
+function(cvx_build_example target)
     add_executable(${target} ${target}.cpp ${CVX_SOURCES})
     set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/examples/bin)
 
-    if(needs_opencv AND OpenCV_FOUND)
+    if(${CVX_WITH_OPENCV} AND OpenCV_FOUND)
         target_link_libraries(${target} ${OpenCV_LIBS})
     endif()
 endfunction()
 
 # Function for building a cvx test
-function(cvx_build_test target needs_opencv)
+function(cvx_build_test target)
     add_executable(${target} ${target}.cpp ${CVX_SOURCES})
     set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/tests/bin)
 
-    if(needs_opencv AND OpenCV_FOUND)
+    if(${CVX_WITH_OPENCV} AND OpenCV_FOUND)
         target_link_libraries(${target} ${OpenCV_LIBS})
     endif()
 
